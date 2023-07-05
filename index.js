73f5df81568388f05c8d29e1d85bbe39a2f0597b
BennyInokp <inokpuareakhilome@gmail.com>
2023-07-05 03:45:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express"); 
+const app = express();
+const path = require("path");
+
+//This is the middle well
+app.use(express.json())
+
+const result = []
+app. get("",(( request, response, next) => {
+     const dir = path.join(_dirname + "/public/index.html")
+     return response.sendFile(dir)
+}));
+
+app.get("/getAll",((request, response) => {
+     response.json({result})
+}));
+
+app.post ("/addition", function(req,res){
+    const{value1, value2} = req.body;
+    const answer = value1 + value2
+    const message = 'the sum of ${value1} and ${value2} is ${answer}'
+    result.push(message)
+    res.json({message})
+})
+
+app.post ("/division", function(req,res){
+    const {value1, value2} = req.body;
+    const answer = value1 / value2
+    res.json({"message":`the sum of ${value1} and ${value2} is ${answer}`})
+    
+})
+
+app.post ("/multiplication", function(req,res){
+    const {value1, value2} = req.body;
+    const answer = value1 * value2
+    res.json({"message":`the sum of ${value1} and ${value2} is ${answer}`})
+    
+})
+
+app.post ("/substraction", function(req,res){
+    const {value1, value2} = req.body;
+    const answer = value1 - value2
+    res.json({"message":`the sum of ${value1} and ${value2} is ${answer}`})
+    
+})
+
+app.listen(5000, function(){
+    console.log("server running on port 5000")
+});
+
+
+
+
+
+
+
+
+
+
